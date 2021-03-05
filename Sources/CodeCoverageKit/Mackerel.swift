1eea2c8fb5c46005ac603ea76a67766d86cf29f8
yutailang0119 <yutailang0119@gmail.com>
2021-03-05 18:37:44 +0900

    Add Mackerel.MackerelError

---
+++ Sources/CodeCoverageKit/Mackerel.swift
@@ -0,0 +1,27 @@
+//
+//  Mackerel.swift
+//  CodeCoverageKit
+//
+//  Created by Yutaro Muta on 2021/03/05.
+//
+
+import Foundation
+
+public enum Mackerel {
+    public enum MackerelError: Error {
+        case forbidden
+        case tooManyRequests
+        case any
+
+        init(statusCode: Int) {
+            switch statusCode {
+            case 403:
+                self = .forbidden
+            case 429:
+                self = .tooManyRequests
+            default:
+                self = .any
+            }
+        }
+    }
+}
