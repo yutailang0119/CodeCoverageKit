dd85ca0315bd0ea4acf799bf4945eaf64c872767
yutailang0119 <yutailang0119@gmail.com>
2021-03-08 22:44:57 +0900

    Remove unnecessary test codes

--- Tests/CodeCoverageKitTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(swift_code_coverage_mackerelTests.allTests),
-    ]
-}
-#endif
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import swift_code_coverage_mackerelTests
-
-var tests = [XCTestCaseEntry]()
-tests += swift_code_coverage_mackerelTests.allTests()
-XCTMain(tests)
