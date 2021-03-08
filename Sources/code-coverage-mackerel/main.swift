51bacd0a54c9ab71f5aa72caf58cff83ea214b0d
yutailang0119 <yutailang0119@gmail.com>
2021-03-08 22:43:33 +0900

    Fix main.swift to implement

--- Sources/code-coverage-mackerel/main.swift
+++ Sources/code-coverage-mackerel/main.swift
@@ -1,1 +1,32 @@
-print("Hello, world!")
+import Foundation
+import CodeCoverageKit
+
+let environment = Environment()
+guard let data = FileManager.default.contents(atPath: environment.codecovFilePath) else {
+    fatalError("")
+}
+
+do {
+    let codecov = try JSONDecoder().decode(CodeCov.self, from: data)
+    guard let coverage = codecov.data.first else {
+        print("Not Found to code coverage")
+        exit(1)
+    }
+    let action = MackerelAction(session: URLSession.shared,
+                                apiKey: environment.apiKey,
+                                userAgent: environment.userAgent,
+                                serverURL: environment.serverURL,
+                                serviceName: environment.serviceName,
+                                graphName: environment.graphName,
+                                coverage: coverage)
+    action.run { result in
+        switch result {
+        case .success:
+            print("Success")
+        case .failure(let error):
+            print(error)
+        }
+    }
+} catch {
+    print(error)
+}
