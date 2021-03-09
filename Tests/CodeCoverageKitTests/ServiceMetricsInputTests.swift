1c367665413d2ecdcf16fdc9a10aef375a3e8a4d
yutailang0119 <yutailang0119@gmail.com>
2021-03-09 14:26:24 +0900

    Fix tests

--- Tests/CodeCoverageKitTests/ServiceMetricsInputTests.swift
+++ Tests/CodeCoverageKitTests/ServiceMetricsInputTests.swift
@@ -11,12 +11,12 @@ import XCTest
 final class ServiceMetricsInputTests: XCTestCase {
     func testEncodeAsArray() throws {
         let date = Date()
-        let metrics: [Mackerel.ServiceMetricsInput.Metric] = [
+        let metrics: [ServiceMetricsAction.Input.Metric] = [
             .init(name: "foo", time: date, value: 10),
             .init(name: "bar", time: Date(timeInterval: -60 * 60, since: date), value: 0.1),
             .init(name: "baz", time: Date(timeInterval: -60 * 60 * 60, since: date), value: 11.11),
         ]
-        let input = Mackerel.ServiceMetricsInput(metrics: metrics)
+        let input = ServiceMetricsAction.Input(metrics: metrics)
         let encoded = try JSONEncoder().encode(input)
 
         let json = String(data: encoded, encoding: .utf8)
@@ -24,13 +24,13 @@ final class ServiceMetricsInputTests: XCTestCase {
     }
 }
 
-private extension Mackerel.ServiceMetricsInput {
+private extension ServiceMetricsAction.Input {
     var json: String {
         "[\(metrics.map(\.json).joined(separator: ","))]"
     }
 }
 
-private extension Mackerel.ServiceMetricsInput.Metric {
+private extension ServiceMetricsAction.Input.Metric {
     var json: String {
         "{\"name\":\"\(name)\",\"time\":\(time.timeIntervalSince1970),\"value\":\(value)}"
     }
